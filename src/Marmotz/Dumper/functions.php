d425ad85d48bb3f871c66225db72399ea37e45e8
Renaud LITTOLFF <renaud@atipik.fr>
2013-07-24 11:40:55 +0200

    add getDump helper

--- src/Marmotz/Dumper/functions.php
+++ src/Marmotz/Dumper/functions.php
@@ -2,8 +2,6 @@
 
 /**
  * helper to dump variables
- *
- * @return string
  */
 function dump(&$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
               &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
@@ -20,4 +18,29 @@ function dump(&$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = nu
         $dumper->dump($args[$key]);
         echo PHP_EOL;
     }
+}
+
+/**
+ * helper to get variables dump
+ *
+ * @return string
+ */
+function getDump(&$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null)
+{
+    $dumper = Marmotz\Dumper\Dump::factory();
+
+    $args = debug_backtrace();
+    $args = $args[0]['args'];
+
+    $dump = '';
+
+    foreach (array_keys($args) as $key) {
+        $dump .= $dumper->getDump($args[$key]) . PHP_EOL;
+    }
+
+    return $dump;
 }
