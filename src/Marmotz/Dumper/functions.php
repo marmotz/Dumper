e2a1595b5a43213f1b5332e1edb4ee1c0ad73577
marmotz <renaud@gmail.com>
2013-07-25 11:24:06 +0200

    Add dumpd function

--- src/Marmotz/Dumper/functions.php
+++ src/Marmotz/Dumper/functions.php
@@ -21,6 +21,28 @@ function dump(&$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = nu
 }
 
 /**
+ * helper to dump variables and die
+ */
+function dumpd(&$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null,
+              &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null, &$arg = null)
+{
+    $dumper = Marmotz\Dumper\Dump::factory();
+
+    $args = debug_backtrace();
+    $args = $args[0]['args'];
+
+    foreach (array_keys($args) as $key) {
+        $dumper->dump($args[$key]);
+        echo PHP_EOL;
+    }
+
+    die;
+}
+
+/**
  * helper to get variables dump
  *
  * @return string
