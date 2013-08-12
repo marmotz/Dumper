b687f3572a82a793b6592ff844f48298f4d62931
marmotz <rlittolff@gmail.com>
2013-08-12 23:50:07 +0200

    Fx atoum bootstrap

--- .atoum.php
+++ .atoum.php
@@ -6,21 +6,20 @@ define('CODE_COVERAGE_ROOT', TEST_ROOT . DIRECTORY_SEPARATOR . 'coverage');
 
 $report = $script->addDefaultReport();
 
-if (!file_exists(CODE_COVERAGE_ROOT)) {
-    mkdir(CODE_COVERAGE_ROOT);
+if (!file_exists(CODE_COVERAGE_ROOT) && !@mkdir(CODE_COVERAGE_ROOT)) {
+    die('Unable to create directory "' . CODE_COVERAGE_ROOT . '".');
 }
 
 $coverageField = new atoum\report\fields\runner\coverage\html(
-    'Dumper',
+    basename(__DIR__),
     CODE_COVERAGE_ROOT
 );
 
 $coverageField->setRootUrl('file://' . CODE_COVERAGE_ROOT);
 
+$coverageField->addSrcDirectory(__DIR__ . '/src/Marmotz/Dumper');
+
 $report->addField($coverageField);
 
 
 $script->noCodeCoverageForNamespaces('atoum');
-
-
-$script->bootstrapFile(TEST_ROOT . DIRECTORY_SEPARATOR . 'bootstrap.php');
